48d180cdf5b0ff18b672aad6e59bb9ecd1f4f5d8
Ashish Shrestha <aashishshrestha5532@gmail.com>
2021-01-07 18:00:27 +0545

    fixed some bugs

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Codesprite!!!!!");
+const helper = {
+  /**
+   * @param a first param
+   * @param b second param
+   */
+  add: (a, b) => {
+    return a + b;
+  },
+};
+
+module.exports = helper;
